697c9e00a08b0ea96272106ae9c81fda9c478722
Patrick Williams <pwilliams0802@gmail.com>
2019-04-11 11:02:41 -0400

    Add JS to limit card to either image or text

--- Capstone/wwwroot/js/Decks/AddCard.js
+++ Capstone/wwwroot/js/Decks/AddCard.js
@@ -2,6 +2,10 @@
 const tagDiv = document.querySelector('#tags');
 const firstTag = document.querySelector('#Tags_0_');
 const addTagButton = document.querySelector('#AddNewTag');
+const getFrontTextbox = document.getElementById('front-text').form[0];
+const getImageTextbox = document.getElementById('front-text').form[1];
+const frontTextboxPlaceholder = getFrontTextbox.placeholder;
+const imageTextboxPlaceholder = getImageTextbox.placeholder;
 
 addTagButton.addEventListener('click', e => {
     console.log('typed');
@@ -16,12 +20,26 @@ addTagButton.addEventListener('click', e => {
     //newTag.addEventListener('oninput', AddNewTag());
 });
 
-//function AddNewTag() {
-//    console.log('typed');
-//    const newTag = document.createElement('input');
-//    newTag.className = 'form-control';
-//    newTag.type = 'text';
-//    newTag.id = `Tags_${tagCount}_`;
-//    newTag.name = `Tags[${tagCount}]`;
-//    //newTag.addEventListener('oninput', AddNewTag());
-//}
+getFrontTextbox.addEventListener('input', i => {
+    if (getFrontTextbox.value !== '') {
+        getImageTextbox.disabled = true
+        getImageTextbox.placeholder = "You can only have an image OR text"
+    }
+    else {
+        getImageTextbox.disabled = false
+        getImageTextbox.placeholder = imageTextboxPlaceholder
+    }
+});
+
+getImageTextbox.addEventListener('input', i => {
+    if (getImageTextbox.value !== '') {
+        
+        getFrontTextbox.disabled = true
+        getFrontTextbox.placeholder = "You can only have an image OR text"
+    }
+    else {
+        getFrontTextbox.disabled = false
+        getFrontTextbox.placeholder = frontTextboxPlaceholder
+    }
+});
+
