ea84545ef08986c41e0e2f9e9a1adb717b68c59f
Josiah Sayers <josiah.sayers15@gmail.com>
2019-04-11 09:37:23 -0400

    Add const variables for elements on study index in js. Add css for study

--- Capstone/wwwroot/js/Study/Index.js
+++ Capstone/wwwroot/js/Study/Index.js
@@ -1,6 +1,15 @@
 ﻿const deckId = document.querySelector('input#deckId').value;
 const apiUrl = `http://localhost:${location.port}/API/`;
 
+const frontOfCard = document.querySelector('div#study-card-front');
+const backOfCard = document.querySelector('div#study-card-back');
+const scoreTracker = document.querySelector('div#score-tracker');
+const correctButton = document.querySelector('div#correct-button');
+const wrongButton = document.querySelector('div#wrong-button');
+const scoreDisplay = document.querySelector('div#score-count p');
+
+console.log(scoreDisplay);
+
 fetch(`${apiUrl}getdeck?id=${deckId}`)
     .then(response => {
         response.json()
