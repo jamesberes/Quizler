018effe952a674feca4bb3bc9e78aa85e4eddebd
Josiah Sayers <josiah.sayers15@gmail.com>
2019-04-11 14:08:38 -0400

    Add custom validation to Add Card

--- Capstone/wwwroot/js/Decks/AddCard.js
+++ Capstone/wwwroot/js/Decks/AddCard.js
@@ -4,42 +4,34 @@ const firstTag = document.querySelector('#Tags_0_');
 const addTagButton = document.querySelector('#AddNewTag');
 const getFrontTextbox = document.getElementById('front-text').form[0];
 const getImageTextbox = document.getElementById('front-text').form[1];
+const form = document.getElementById('add-card-form');
 const frontTextboxPlaceholder = getFrontTextbox.placeholder;
 const imageTextboxPlaceholder = getImageTextbox.placeholder;
-
-addTagButton.addEventListener('click', e => {
-    console.log('typed');
-    const newTag = document.createElement('input');
-    newTag.className = 'form-control';
-    newTag.type = 'text';
-    newTag.id = `Tags_${tagCount}_`;
-    newTag.name = `Tags[${tagCount}]`;
-    
-    tagDiv.appendChild(newTag);
-    tagCount++;
-    //newTag.addEventListener('oninput', AddNewTag());
-});
+const submitButton = document.querySelector('[type=submit]');
 
 getFrontTextbox.addEventListener('input', i => {
     if (getFrontTextbox.value !== '') {
         getImageTextbox.disabled = true
         getImageTextbox.placeholder = "You can only have an image OR text"
+        submitButton.disabled = false;
     }
     else {
         getImageTextbox.disabled = false
         getImageTextbox.placeholder = imageTextboxPlaceholder
+        submitButton.disabled = true;
     }
 });
 
 getImageTextbox.addEventListener('input', i => {
     if (getImageTextbox.value !== '') {
-        
+
         getFrontTextbox.disabled = true
         getFrontTextbox.placeholder = "You can only have an image OR text"
+        submitButton.disabled = false;
     }
     else {
         getFrontTextbox.disabled = false
         getFrontTextbox.placeholder = frontTextboxPlaceholder
+        submitButton.disabled = true;
     }
 });
-
