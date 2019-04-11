e9e67f4dc51a6397d28045eddeb18c0e79683fd9
Patrick Williams <pwilliams0802@gmail.com>
2019-04-11 17:36:51 -0400

    Change view of SearchResult to display cards instead of list

--- Capstone/wwwroot/js/Decks/SearchResults.js
+++ Capstone/wwwroot/js/Decks/SearchResults.js
@@ -1,5 +1,19 @@
-﻿let hiddenInputs = document.querySelectorAll(".hidden-cardId");
+﻿const cards = document.querySelectorAll('div.card');
+let hiddenInputs = document.querySelectorAll(".submit-the-card-id");
+
 hiddenInputs.forEach(input => {
-    let cardId = input.parentElement.parentElement.parentElement.querySelector(".td-cardId").innerText
-    input.value = cardId;
+    input.value = input.parentElement.parentElement.children[0].children[0].value;
 })
+
+
+cards.forEach(card => {
+    card.addEventListener('click', event => {
+        let clickedCard = event.path[1];
+        if (event.target.className == 'card') {
+            clickedCard = event.target;
+        }
+
+        clickedCard.children[1].classList.toggle('hidden');
+        clickedCard.children[2].classList.toggle('hidden');
+    });
+});
