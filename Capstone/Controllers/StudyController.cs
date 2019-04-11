b5ed92e4203a975ebaad4e11412b7e05034f553d
Josiah Sayers <josiah.sayers15@gmail.com>
2019-04-11 09:03:32 -0400

    Add Study controller. Add skeleton code to study.index. add javascript fetch to study.index

---
+++ Capstone/Controllers/StudyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+using Capstone.Models.DALs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class StudyController : Controller
+    {
+        private IDeckDAL decksSqlDAL;
+
+        public StudyController(IDeckDAL decksSqlDAL)
+        {
+            this.decksSqlDAL = decksSqlDAL;
+        }
+
+        public IActionResult Index(int deckId)
+        {
+            return View(deckId);
+        }
+    }
+}
---
+++ Capstone/wwwroot/js/Study/Index.js
@@ -0,0 +1,10 @@
+﻿const deckId = document.querySelector('input#deckId').value;
+const apiUrl = 'http://localhost:62451/API/';
+
+fetch(`${apiUrl}getdeck?id=${deckId}`)
+    .then(response => {
+        response.json()
+            .then(data => {
+                console.log(data);
+            });
+    });
