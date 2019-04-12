4ad8aaf3e72ed2b071844c0d2458a8a452325caa
Josiah Sayers <josiah.sayers15@gmail.com>
2019-04-12 19:29:51 -0400

    Fixed Lazy Loading security vulnerability. The old method left us open to code injection

--- Capstone/wwwroot/js/LazyLoad.js
+++ Capstone/wwwroot/js/LazyLoad.js
@@ -9,22 +9,46 @@ function LazyLoad(apiUrl, container) {
                 if (data.length < 10) {
                     preloader.classList.add('hidden');
                 }
-
+                
                 data.forEach(item => {
                     let deck = document.createElement('a');
                     deck.classList.add('deck-link');
                     deck.href = `/Decks/ViewDeck?deckId=${item.id}`;
-                    deck.innerHTML = `<div class="deck" id="${item.id}">` +
-                        `<h1>${item.name}</h1>` +
-                        `<p>${item.description}</p>` +
-                        '</div>'
+
+                    let deckDiv = document.createElement('div');
+                    deckDiv.classList.add('deck');
+                    deckDiv.id = `${item.id}`;
+
+                    let h1 = document.createElement('h1');
+                    h1.innerText = item.name;
+
+                    let p = document.createElement('p');
+                    p.innerText = item.description;
+
                     maxLoadedDeckId = item.id;
+
+                    deckDiv.appendChild(h1);
+                    deckDiv.appendChild(p);
+
+                    deck.appendChild(deckDiv);
+
                     container.appendChild(deck);
+
+                    //let deck = document.createElement('a');
+                    //deck.classList.add('deck-link');
+                    //deck.href = `/Decks/ViewDeck?deckId=${item.id}`;
+                    //deck.innerHTML = `<div class="deck" id="${item.id}">` +
+                    //    `<h1>${item.name}</h1>` +
+                    //    `<p>${item.description}</p>` +
+                    //    '</div>'
+                    //maxLoadedDeckId = item.id;
+                    //container.appendChild(deck);
                 })
                 resultCount = data.length;
                 if (resultCount > 9) {
                     window.addEventListener('scroll', LazyLoad(apiUrl, container));
                 }
+                return data;
             })
         });
 }
