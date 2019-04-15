eee4dbecf1cb91277e5f9c23067d786c286b1354
Patrick Williams <pwilliams0802@gmail.com>
2019-04-15 10:14:37 -0400

    Add ViewAllAdminCards method

--- Capstone/Models/DALs/CardSqlDAL.cs
+++ Capstone/Models/DALs/CardSqlDAL.cs
@@ -97,6 +97,43 @@ namespace Capstone.Models.DALs
 
         }
 
+        public List<Card> ViewAllAdminCards()
+        {
+            List<Card> result = new List<Card>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql_GetAllAdminCards, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Card card = new Card
+                        {
+                            Id = Convert.ToInt32(reader["id"]),
+                            Front = Convert.ToString(reader["front"]),
+                            Back = Convert.ToString(reader["back"]),
+                            ImageURL = Convert.ToString(reader["img"]),
+                            DeckId = Convert.ToInt32(reader["deck_id"]),
+                            CardOrder = Convert.ToInt32(reader["card_order"])
+                        };
+
+                        card.Tags = tagSqlDAL.GetTagsForCard(card.Id);
+
+                        result.Add(card);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                result = new List<Card>();
+            }
+            return result;
+        }
+
         public Card UpdateCard(Card updatedCard)
         {
             Card output;
